76a932324c2fa0f0edef8ff4e220f61934db7ebc
Durgesh-0001 <166849105+Durgesh-0001@users.noreply.github.com>
2025-06-19 12:03:11 +0530

    Update index.js
    Cors error correction

--- src/index.js
+++ src/index.js
@@ -18,12 +18,22 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://duva-three.vercel.app",
+];
 app.use(
   cors({
-    origin: "http://localhost:5173", // Allow all origins (for testing)
-    credentials: true, // Allow cookies if using JWT
-    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
-    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
